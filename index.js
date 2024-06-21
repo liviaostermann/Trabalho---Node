6c7d08512beb593a0d532e9637941e74c48cd475
DudaMewes <>
2024-06-21 08:53:53 -0300

    Código - req Meta

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ banco.conexao.sync(function () {
   console.log("Banco de dados conectado.");
 });
 
-//pegar atleta pelo id 
+//pegar atleta pelo id
 app.get("/atletas/:id", async function (req, res) {
   const resultado = await atleta.atleta.findByPk(req.params.id);
   if (resultado == null) {
@@ -67,6 +67,64 @@ app.put("/atletas/:id", async function (req, res) {
 
 app.delete("/atletas/:id", async function (req, res) {
   const resultado = await atleta.atleta.destroy({
+    where: { id: req.params.id },
+  });
+  if (resultado == null) {
+    res.status(404).send({});
+  } else {
+    res.status(204).send({});
+  }
+});
+
+//REQ METAS
+app.get("/metas/", async function (req, res) {
+  const resultado = await meta.meta.findAll();
+  res.send(resultado);
+});
+
+banco.conexao.sync(function () {
+  console.log("Banco de dados conectado.");
+});
+
+app.get("/metas/:id", async function (req, res) {
+  const resultado = await meta.meta.findByPk(req.params.id);
+  if (resultado == null) {
+    res.status(404).send({});
+  } else {
+    res.send(resultado);
+  }
+});
+
+app.post("/metas/", async function (req, res) {
+  const resultado = await meta.meta.create({
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+  });
+  res.send(resultado);
+});
+
+app.put("/metas/:id", async function (req, res) {
+  const resultado = await meta.meta.update(
+    {
+      //primeiro parametro = atributos
+      //body = corpo da requisição no insomnia
+      titulo: req.body.titulo,
+      descricao: req.body.descricao,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  );
+
+  if (resultado == 0) {
+    res.status(404).send({});
+  } else {
+    res.send(await meta.meta.findByPk(req.params.id));
+  }
+});
+
+app.delete("/metas/:id", async function (req, res) {
+  const resultado = await meta.meta.destroy({
     where: { id: req.params.id },
   });
   if (resultado == null) {
